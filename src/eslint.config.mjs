73274398566ffec2049db1d0ea601fab2d1a71df
Jalu Tri Atmaja <jalutriatmaja19@gmail.com>
2024-11-10 10:08:13 +0700

    initialcommit

---
+++ src/eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import daStyle from 'eslint-config-dicodingacademy';
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  daStyle,
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
